f1d56a42a8312db9505805dccfa37a2211dbca7e
GioGKS <gulikashvili205@gmail.com>
2022-03-29 22:07:39 +0300

    File/IO

---
+++ HMW_FileIO/Car.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace HMW_FileIO
+{
+    [XmlRoot("Sport_Car")]
+    public class Car
+    {
+        //Fields
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Codan { get; set; }
+        public int NumberOfSeats { get; set; }
+
+        //Constructors
+        public Car()
+        {
+        }
+
+        public Car(string model,string brand,string color,int year,int codan,int numOfSeats)
+        {
+            Model = model;
+            Brand = brand;
+            Color = color;
+            Year = year;
+            Codan = codan;
+            NumberOfSeats = numOfSeats;
+        }
+
+
+        //Part B | Challenge - Constructor
+        public Car(Stream stream)
+        {
+            Car car = new();
+            XmlSerializer firstXml = new XmlSerializer(typeof(Car));
+            firstXml.Serialize(stream, car);
+        }
+
+
+        //Methods
+        public int GetCodan(int carCode)
+        {
+            Codan = carCode;
+            return carCode;
+        }
+
+        public int GetNumberOfSeats(int seats)
+        {
+            NumberOfSeats = seats;
+            return NumberOfSeats;
+        }
+
+        public void ChangeNumberOfSeats(int newNumberOfSeats)
+        {
+            NumberOfSeats = newNumberOfSeats;
+        }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        //Part B | Challenge - Methods
+        public static void SerilizeACar(Stream stream,Car car)
+        {
+            XmlSerializer xml11 = new XmlSerializer(typeof(Car));
+            xml11.Serialize(stream, car);
+        }
+
+        public static void SerilizeACarArray(Stream stream, Car[] carArr)
+        {
+            XmlSerializer xml11 = new XmlSerializer(typeof(Car));
+            xml11.Serialize(stream, carArr);
+        }
+
+        
+        
+
+        
+
+    }
+}
---
+++ HMW_FileIO/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+namespace HMW_FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Excersice 16
+            
+            Console.WriteLine("---------------------------Excercise 16---------------------------");
+
+            //Write First 10 Files *USING MACBOOK* - (This Is Not Same Way As Windows)
+            int i = 0;
+            foreach (string files in Directory.EnumerateDirectories(@"/users/gio"))
+            {
+                i++;
+                Console.WriteLine(files);
+                if (i == 10)
+                {
+                    i = 0;
+                    break;
+                }
+            }
+            
+            #endregion
+
+            #region Excersice 17
+
+            string newPath = @"/users/gio/desktop/FormulaCar.txt";
+            Car Formula1 = new Car();
+            Car[] carsArray = new Car[5];
+
+            XmlSerializer xml = new XmlSerializer(typeof(Car));
+            using (Stream streamForCar = new FileStream(newPath, FileMode.Create))
+            {
+                xml.Serialize(streamForCar, Formula1);
+                Formula1.Brand = "BMW";
+                Formula1.Color = "Black";
+                Formula1.Model = "M8 Competition";
+                Formula1.NumberOfSeats = 2;
+                Formula1.Codan = 7717;
+                Formula1.Year = 2019;
+            }
+
+            //Check If XML Is Changing After Updating Number Of Seats
+            Formula1.ChangeNumberOfSeats(8);
+            Console.WriteLine($"Number Of Seats After Changing:\n{Formula1.NumberOfSeats}");
+
+            Stream path = new FileStream(@"/users/gio/desktop/FormulaCar.txt", FileMode.Open);
+            Car newCar = new Car(path);
+
+
+
+            #endregion
+        }
+    }
+}
